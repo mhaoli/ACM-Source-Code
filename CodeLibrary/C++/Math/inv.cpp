f10d3b7fec887d90a2939914f1434a683faadde9
Merlinhool <Merlinhool1994@gmail.com>
2015-04-27 16:17:51 +0800

    Updated inv.cpp

--- CodeLibrary/C++/Math/inv.cpp
+++ CodeLibrary/C++/Math/inv.cpp
@@ -9,13 +9,14 @@
 //inv[i] i的逆元
 //fac_inv[i] = i!的逆元
 
-const int mod = ; //mod is prime
+int fac[Max_N], inv[Max_N], fac_inv[Max_N];
 
-void inv_init(int n) {
+void init(int n) {
         fac_inv[0] = fac_inv[1] = inv[1] = fac[1] = 1;
-        for(int i = 2; i <= N; ++i) {
+        for(int i = 2; i <= n; ++i) {
                 fac[i] = (long long)fac[i-1] * i % mod;
                 inv[i] = (long long)(mod - mod / i) * inv[mod%i] % mod;
                 fac_inv[i] = (long long)fac_inv[i-1] * inv[i] % mod;
         }
 }
+
