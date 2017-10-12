6bb179e3c5ec955042d7796287d06033aa27504f
Minghao Li <Merlinhool1994@gmail.com>
2017-10-13 00:01:43 +0800

    modify geometry.cpp

--- CodeLibrary/C++/Geometry/Geometry.cpp
+++ CodeLibrary/C++/Geometry/Geometry.cpp
@@ -1,41 +1,83 @@
-struct Point {
+namespace doubleGeometry {
+    const double EPS = 1e-5;
+    int eps(e){if(fabs(e)<EPS)return 0;if(e>EPS)return 1;return -1;}
+    //head 
+    struct Point {
+        double x, y;
+        Point(double x = 0, double y = 0) : x(x), y(y){ }
+        void read() {
+            scanf("%lf%lf", &x, &y);
+        }
+        void output() {
+            printf("x:%f y:%f\n", x, y);
+        }
+        int dim() const {
+            return !(y > 0 || (y == 0 && x >= 0));
+        }
+    };
+    typedef Point Vector;
+    Point operator - (const Point &a, const Point &b) {
+        return Point(a.x - b.x, a.y - b.y);
+    }
+    Point operator + (const Point &a, const Point &b) {
+        return Point(a.x + b.x, a.y + b.y);
+    }
+    bool operator == (const Point &a, const Point &b) {
+        return !eps(a.x - b.x) && !eps(a.y - b.y);
+    }
+    bool operator != (const Point &a, const Point &b) {
+        return !(a == b);
+    }
+    //数量积
+    Point operator * (const Point &a, int p) {    
+        return Point(a.x * p, a.y * p);
+    }
+    //叉乘
+    double operator ^ (const Point &a, const Point &b) {
+        return a.x * b.y - a.y * b.x;
+    }
+    bool operator < (const Point &a, const Point &b) {
+        return a.dim() < b.dim() || (a.dim() == b.dim() && eps(a^b) > 0);
+    }
+}
+
+namespace intGeometry {
+    struct Point {
         int x, y;
         Point(int x = 0, int y = 0) : x(x), y(y){ }
         void read() {
-                scanf("%d%d", &x, &y);
+            scanf("%d%d", &x, &y);
         }
         void output() {
-                printf("%d %d\n", x, y);
+            printf("x:%d y:%d\n", x, y);
         }
         int dim() const {
-                return y > 0 || (y == 0 && x >= 0);
+            return !(y > 0 || (y == 0 && x >= 0));
         }
-};
-typedef Point Vector;
-Point operator - (const Point &a, const Point &b) {
+    };
+    typedef Point Vector;
+    Point operator - (const Point &a, const Point &b) {
         return Point(a.x - b.x, a.y - b.y);
-}
-Point operator + (const Point &a, const Point &b) {
+    }
+    Point operator + (const Point &a, const Point &b) {
         return Point(a.x + b.x, a.y + b.y);
-}
-bool operator == (const Point &a, const Point &b) {
-        return a.x == b.x && a.y == b.y;
-}
-bool operator != (const Point &a, const Point &b) {
+    }
+    bool operator == (const Point &a, const Point &b) {
+        return !eps(a.x - b.x) && !eps(a.y - b.y);
+    }
+    bool operator != (const Point &a, const Point &b) {
         return !(a == b);
-}
-//数量积
-Point operator * (const Point &a, int p) {    
+    }
+    //数量积
+    Point operator * (const Point &a, int p) {    
         return Point(a.x * p, a.y * p);
+    }
+    //叉乘
+    double operator ^ (const Point &a, const Point &b) {
+        return a.x * b.y - a.y * b.x;
+    }
+    bool operator < (const Point &a, const Point &b) {
+        return a.dim() < b.dim() || (a.dim() == b.dim() && a^b > 0);
+    }
 }
-//叉乘
-long long operator ^ (const Point &a, const Point &b) {
-        return (long long)a.x * b.y - (long long)a.y * b.x;
-}
-bool operator < (const Point &a, const Point &b) {
-        if(a.dim() != b.dim()) {
-                return a.dim() > b.dim();
-        } else {
-                return (a ^ b) > 0;
-        }
-}
+
